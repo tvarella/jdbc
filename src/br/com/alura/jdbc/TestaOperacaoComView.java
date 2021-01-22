7f9c95eb6b7b7b2def5e699f3d80a552cbea2b42
Varella <thiago_varella@yahoo.com>
2021-01-22 17:01:47 -0200

    tentando novamente

--- src/br/com/alura/jdbc/TestaOperacaoComView.java
+++ src/br/com/alura/jdbc/TestaOperacaoComView.java
@@ -9,5 +9,7 @@ public class TestaOperacaoComView {
 	public static void main(String[] args) {
 		ProdutoCategoriaFrame produtoCategoriaFrame = new ProdutoCategoriaFrame();
 		produtoCategoriaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		System.out.println("teste do tio");
 	}
 }
